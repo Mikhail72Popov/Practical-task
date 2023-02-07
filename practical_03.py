7ae89c7fb504d1019d244947a08372fa4df48d2b
Mikhail <Popov.Mikhail.A@gmail.com>
2023-02-08 00:21:34 +0600

    Задание третьего семинара

--- practical_03.py
+++ practical_03.py
@@ -74,26 +74,26 @@
 # 12
 
 
-import re
-print("Введите текст: ")
-def isCyrillic(text):
-	return bool(re.search('[а-яА-Я]', text))
-points_en = {1:'AEIOULNSTR',
-      	2:'DG',
-      	3:'BCMP',
-      	4:'FHVWY',
-      	5:'K',
-      	8:'JX',
-      	10:'QZ'}
-points_ru = {1:'АВЕИНОРСТ',
-      	2:'ДКЛМПУ',
-      	3:'БГЁЬЯ',
-      	4:'ЙЫ',
-      	5:'ЖЗХЦЧ',
-      	8:'ШЭЮ',
-      	10:'ФЩЪ'}
-text = input().upper()
-if isCyrillic(text):
-	print(sum([k for i in text for k, v in points_ru.items() if i in v]))
-else:
-	print(sum([k for i in text for k, v in points_en.items() if i in v]))
+# import re
+# print("Введите текст: ")
+# def isCyrillic(text):
+# 	return bool(re.search('[а-яА-Я]', text))
+# points_en = {1:'AEIOULNSTR',
+#       	2:'DG',
+#       	3:'BCMP',
+#       	4:'FHVWY',
+#       	5:'K',
+#       	8:'JX',
+#       	10:'QZ'}
+# points_ru = {1:'АВЕИНОРСТ',
+#       	2:'ДКЛМПУ',
+#       	3:'БГЁЬЯ',
+#       	4:'ЙЫ',
+#       	5:'ЖЗХЦЧ',
+#       	8:'ШЭЮ',
+#       	10:'ФЩЪ'}
+# text = input().upper()
+# if isCyrillic(text):
+# 	print(sum([k for i in text for k, v in points_ru.items() if i in v]))
+# else:
+# 	print(sum([k for i in text for k, v in points_en.items() if i in v]))
